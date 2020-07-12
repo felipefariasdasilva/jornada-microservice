b38eb2265da47fcc14f618976dc7293ea67bceb8
felipefariasdasilva <feliipefarias@outlook.com>
2020-07-12 16:02:06 -0300

    deleted order classes because it doesnt work

--- consumer-service/src/main/java/com/felipe/consumer/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.felipe.consumer.repository;
-
-import com.felipe.consumer.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderRepository extends JpaRepository<Order, UUID> {
-}
